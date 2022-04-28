8f3d3c0342f28dc7d313c431955606c1f7d03971
KevinLeNozer <kevin.lenozer@gmail.com>
2022-04-28 14:30:07 +0200

    test ok

--- tpUnitaire/test/panier.test.js
+++ tpUnitaire/test/panier.test.js
@@ -1,7 +1,14 @@
 import should from 'should';
 
-import {calculeTva} from "../app";
-import {createProduit} from "../app";
-import {prixTotal} from "../app";
+import {calculeTva} from "../app.js";
+import {createProduit} from "../app.js";
+import {prixTotal} from "../app.js";
 
 
+describe("Checking if the panier is empty", function(){
+        it("calcul tva", function(){
+                debugger
+                console.log(prixTotal());
+                console.log(calculeTva());
+            });
+    });
